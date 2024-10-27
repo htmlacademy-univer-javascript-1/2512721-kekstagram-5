3af3571374a6306b4e6c26ba38f36376aeee906d
rrrrrrrrrrr00 <Viktoria.Gavrilova@urfu.me>
2024-10-27 16:01:31 +0500

    Модуляция

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import {"./data.js"};
-import {"./util.js"};
+import "./data.js";
+import "./util.js";
 import { createPhotoDescription } from "./data.js";
 const photoDescriptions = Array.from({length: 25}, createPhotoDescription);
