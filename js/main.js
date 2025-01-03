e8b40d519bd5f37fadefd14077bf1b4c3b8245e1
rrrrrrrrrrr00 <Viktoria.Gavrilova@urfu.me>
2025-01-04 03:02:23 +0500

    Доработки

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import { initEditPopup } from './form.js';
 import { getData } from './api.js';
 import { showAlert } from './message.js';
 import { showFilteredPhotos } from './filter.js';
+import { closeEditPopup, setFormSubmit } from './form.js';
 
 getData()
   .then((thumbnails) => {
@@ -12,5 +13,5 @@ getData()
       showAlert('Проблемы с сервером: не удалось загрузить данные. Попробуйте обновить страницу!');
     }
   );
-
+setFormSubmit(closeEditPopup);
 initEditPopup();
